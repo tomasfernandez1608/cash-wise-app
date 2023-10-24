74db4149b0c38ff062914827571297fe86bba3fe
ramatc <rtanquiascornejo@gmail.com>
2023-10-24 19:47:21 -0300

    Fixeando errores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import { Home } from "./views/Home"
+import Home from "./views/Home"
 import './styles.css';
-// import './assets/demo/chart-area-demo';
 import 'https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js';
 
 const App = () => {

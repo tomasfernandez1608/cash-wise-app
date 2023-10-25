9040ab5331b5d058b364288a8cf027f681c90e56
ramatc <rtanquiascornejo@gmail.com>
2023-10-25 20:20:50 -0300

    Cambiando estilos

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,39 +2,39 @@ const Nav = () => {
     return (
         <div className="nav">
             <div className="sb-sidenav-menu-heading">Core</div>
-            <a className="nav-link" href="index.html">
+            <a className="nav-link text-white" href="index.html">
                 <div className="sb-nav-link-icon">
                     <i className="fas fa-tachometer-alt"></i>
                 </div>
                 Dashboard
             </a>
             <div className="sb-sidenav-menu-heading">Addons</div>
-            <a className="nav-link" href="charts.html">
+            <a className="nav-link text-white" href="charts.html">
                 <div className="sb-nav-link-icon">
                     <i className="fas fa-chart-area"></i>
                 </div>
                 Charts
             </a>
-            <a className="nav-link" href="tables.html">
+            <a className="nav-link text-white" href="tables.html">
                 <div className="sb-nav-link-icon">
                     <i className="fas fa-table"></i>
                 </div>
                 Historial
             </a>
             <div className="sb-sidenav-menu-heading">Nosotros</div>
-            <a className="nav-link" href="charts.html">
+            <a className="nav-link text-white" href="charts.html">
                 <div className="sb-nav-link-icon">
                     <i className="fa-solid fa-star fa-flip"></i>
                 </div>
                 Premium
             </a>
-            <a className="nav-link" href="charts.html">
+            <a className="nav-link text-white" href="charts.html">
                 <div className="sb-nav-link-icon">
                     <i className="fas fa-chart-area"></i>
                 </div>
                 ¿Quienes Somos?
             </a>
-            <a className="nav-link" href="tables.html">
+            <a className="nav-link text-white" href="tables.html">
                 <div className="sb-nav-link-icon">
                     <i className="fas fa-table"></i>
                 </div>
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -42,56 +42,19 @@ const Home = () => {
             <div id="layoutSidenav">
                 <div id="layoutSidenav_nav">
                     <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                        <div className="sb-sidenav-menu">
+                        <div className="sb-sidenav-menu bg-secondary">
                             <Nav />
                         </div>
                     </nav>
                 </div>
+   
                 <div id="layoutSidenav_content">
                     <main>
                         <div className="container-fluid px-4">
-                            <h1 className="mt-4">Dashboard</h1>
+                            <h1 className="mt-4">Cashwise App</h1>
                             <ol className="breadcrumb mb-4">
-                                <li className="breadcrumb-item active">Dashboard</li>
+                                <li className="breadcrumb-item active">Cashwise es una aplicación de gestión de gastos y finanzas personales diseñada para simplificar y fortalecer tu control financiero. Con Cashwise, podes registrar y categorizar fácilmente tus ingresos y gastos, establecer presupuestos personalizados y hacer un seguimiento de tus metas de ahorro. La interfaz intuitiva y las poderosas herramientas de visualización te permiten entender tus patrones de gasto y tomar decisiones financieras más inteligentes. Cashwise es tu socio confiable para alcanzar la estabilidad financiera y asegurarte de que tu dinero trabaje para vos.</li>
                             </ol>
-                            <div className="row">
-                                <div className="col-xl-3 col-md-6">
-                                    <div className="card bg-primary text-white mb-4">
-                                        <div className="card-body">Primary Card</div>
-                                        <div className="card-footer d-flex align-items-center justify-content-between">
-                                            <a className="small text-white stretched-link" href="#">View Details</a>
-                                            <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-xl-3 col-md-6">
-                                    <div className="card bg-warning text-white mb-4">
-                                        <div className="card-body">Warning Card</div>
-                                        <div className="card-footer d-flex align-items-center justify-content-between">
-                                            <a className="small text-white stretched-link" href="#">View Details</a>
-                                            <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-xl-3 col-md-6">
-                                    <div className="card bg-success text-white mb-4">
-                                        <div className="card-body">Success Card</div>
-                                        <div className="card-footer d-flex align-items-center justify-content-between">
-                                            <a className="small text-white stretched-link" href="#">View Details</a>
-                                            <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-xl-3 col-md-6">
-                                    <div className="card bg-danger text-white mb-4">
-                                        <div className="card-body">Danger Card</div>
-                                        <div className="card-footer d-flex align-items-center justify-content-between">
-                                            <a className="small text-white stretched-link" href="#">View Details</a>
-                                            <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                             <div className="row">
                                 <div className="col-xl-6">
                                     <div className="card mb-4">

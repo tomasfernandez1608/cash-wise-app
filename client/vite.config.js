7adc8ce6e381389e50c1fe18dfd5d85dee4ae1f9
ramatc <rtanquiascornejo@gmail.com>
2023-10-24 21:26:08 -0300

    Probando conexion a base de datos

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost/utn/server/', // La dirección de tu servidor
+        changeOrigin: true,
+      }
+    }
+  }
 })

a7ddc417259f6b38cbd9cef94ab1b4214b857781
soldiersnake <mariano.maciasgandulfo@gmail.com>
2023-10-28 14:29:11 -0300

    push de cambio prueba

--- client/src/components/Piechart.jsx
+++ client/src/components/Piechart.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto'
+import Chart from 'chart.js/auto';
 
 const Piechart = () => {
     const categories = ['Comidas y bebidas', 'Supermercado', 'Transporte'];

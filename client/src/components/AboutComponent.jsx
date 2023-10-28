b5f25d79e1cbd9c4068343b0935c2ea308e8f478
tomasfernandez1608 <tomasfernandez1608@hotmail.com>
2023-10-28 14:13:06 -0300

    Creacion Componente About

---
+++ client/src/components/AboutComponent.jsx
@@ -0,0 +1,50 @@
+const AboutComponent = () => {
+  return (
+    <>
+      <main class="bg-white">
+        <div class="row">
+          <div class="col-12 d-flex  justify-content-center ">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/eco-gm.appspot.com/o/Fotos_Producto%2F2.jpg?alt=media&token=5d91ce71-5ef1-479e-8cf2-6d20522048b0&_gl=1*x46w8k*_ga*MjAzOTcxOTU0My4xNjk3MTMwODQ4*_ga_CW55HF8NVT*MTY5ODUwMjc0Mi42LjEuMTY5ODUwMjc3NC4yOC4wLjA."
+              class="card-img-top  w-auto h-auto ml-5"
+              alt="..."
+            ></img>
+            <div class="card m-5 text-white bg-dark ">
+              <div class="card-body">
+                <h5 class="card-title">Acerca de Cash Wise App</h5>
+                <p class="card-text">
+                  Bienvenidos a Cash Wise App, donde creemos que el control
+                  financiero es la base para una vida más segura y próspera.
+                  Nuestra pasión por la gestión inteligente de gastos nos llevó
+                  a desarrollar, una solución innovadora que pone el control de
+                  tus finanzas en tus manos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class=" col-12 d-flex  justify-content-center  align-content-center  ">
+            <div class=""></div>
+            <div class="card m-5 text-white bg-dark ">
+              <div class="card-body">
+                <h5 class="card-title">¿Quienes Somos?</h5>
+                <p class="card-text">
+                  Somos un equipo de expertos en tecnología y finanzas
+                  comprometidos en brindarte las herramientas necesarias para
+                  tomar el control de tus gastos de manera efectiva. Con años de
+                  experiencia en desarrollo de software y una profunda
+                  comprensión de las necesidades financieras de las personas,
+                  creamos Cash Wise App con un enfoque claro: simplificar la
+                  gestión de tus finanzas personales y empresariales.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AboutComponent;

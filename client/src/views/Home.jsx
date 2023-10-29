cdfab3d2bc72a48f739c6b26a277f727d39a7a7c
soldiersnake <mariano.maciasgandulfo@gmail.com>
2023-10-29 15:08:31 -0300

    localeStorage en home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -6,68 +6,101 @@ import CoinRanking from "../components/CoinRanking";
 import { obtenerOperaciones } from "../services/obtenerOperaciones";
 
 const Home = () => {
-    const [operaciones, setOperaciones] = useState([]);
+  const [operaciones, setOperaciones] = useState([]);
 
-    useEffect(() => {
-        async function cargarOperaciones() {
-            try {
-                const operaciones = await obtenerOperaciones();
-                setOperaciones(operaciones);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-
-        cargarOperaciones();
-    }, []);
-
-
-    const [logeado, setLogeado] = useState(false);
-
-    const handleLog = (estado, sessionId) => {
-        setLogeado(estado);
-        console.log('sessionId :', sessionId);
+  useEffect(() => {
+    async function cargarOperaciones() {
+      try {
+        const operaciones = await obtenerOperaciones();
+        setOperaciones(operaciones);
+      } catch (error) {
+        console.log(error);
+      }
     }
 
-    return (
-        <div id="layoutSidenav_content">
-            <main>
-                <div className="container-fluid px-4">
-                    <h1 className="mt-4">Cashwise App</h1>
-                    <ol className="breadcrumb mb-4">
-                        <li className="breadcrumb-item active fs-5">Cashwise es una aplicación de gestión de gastos y finanzas personales diseñada para simplificar y fortalecer tu control financiero. Con Cashwise, podes registrar y categorizar fácilmente tus ingresos y gastos, establecer presupuestos personalizados y hacer un seguimiento de tus metas de ahorro. La interfaz intuitiva y las poderosas herramientas de visualización te permiten entender tus patrones de gasto y tomar decisiones financieras más inteligentes. Cashwise es tu socio confiable para alcanzar la estabilidad financiera y asegurarte de que tu dinero trabaje para vos.</li>
-                    </ol>
-                    <div className="row mb-3">
-                        <div className="col-xl-4">
-                            <div className="text-center">
-                                {
-                                    !logeado ? (
-                                        <Login handleLog={handleLog} />
-                                    ) : (
-                                        
-                                        <button>Cerrar Session</button>
-                                    )
-                                }
-                            </div>
-                        </div>
+    cargarOperaciones();
+  }, []);
 
-                        <div className="col-xl-8">
-                            <div className="card">
-                                <div className="card-header">
-                                    <h5 className="card-title">Balance de gastos</h5>
-                                </div>
-                                <div className="card-body">
-                                    <Piechart operaciones={operaciones} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  const [logeado, setLogeado] = useState(false);
+
+  const handleLog = (estado, sessionId) => {
+    setLogeado(estado);
+
+    console.log("sessionId :", sessionId);
+  };
+
+  useEffect(() => {
+    // logica del localeStorage
+    let user = { name: 'Juan', age: 30 }; //aca armo el usuario deberiamos tomar el objeto q trae desde la API
+    if (logeado) {
+      localStorage.setItem("userInfoWiseApp", JSON.stringify(user)); //almaceno y creo el local storage en este caso se llama 'userInfoWiseApp'
+    }
+  }, [logeado]);
+
+  //FUnciones localeStorage
+  useEffect(() => {
+    // optener data del local
+    const nombreDeUsuario = localStorage.getItem('userInfoWiseApp');
+    console.log(nombreDeUsuario);
+
+    //optener data convertida a string
+    const storedUser = JSON.parse(localStorage.getItem('userInfoWiseApp'));
+    console.log(storedUser);
+
+    // remover item del localStorage
+    localStorage.removeItem('userToken');
+
+    // Eliminar/limpiar localeStorge
+    // localStorage.clear();
+  },[])
+
+  return (
+    <div id="layoutSidenav_content">
+      <main>
+        <div className="container-fluid px-4">
+          <h1 className="mt-4">Cashwise App</h1>
+          <ol className="breadcrumb mb-4">
+            <li className="breadcrumb-item active fs-5">
+              Cashwise es una aplicación de gestión de gastos y finanzas
+              personales diseñada para simplificar y fortalecer tu control
+              financiero. Con Cashwise, podes registrar y categorizar fácilmente
+              tus ingresos y gastos, establecer presupuestos personalizados y
+              hacer un seguimiento de tus metas de ahorro. La interfaz intuitiva
+              y las poderosas herramientas de visualización te permiten entender
+              tus patrones de gasto y tomar decisiones financieras más
+              inteligentes. Cashwise es tu socio confiable para alcanzar la
+              estabilidad financiera y asegurarte de que tu dinero trabaje para
+              vos.
+            </li>
+          </ol>
+          <div className="row mb-3">
+            <div className="col-xl-4">
+              <div className="text-center">
+                {!logeado ? (
+                  <Login handleLog={handleLog} />
+                ) : (
+                  <button>Cerrar Session</button>
+                )}
+              </div>
+            </div>
+
+            <div className="col-xl-8">
+              <div className="card">
+                <div className="card-header">
+                  <h5 className="card-title">Balance de gastos</h5>
                 </div>
-                <CoinRanking />
-            </main>
-            <Footer />
+                <div className="card-body">
+                  <Piechart operaciones={operaciones} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+        <CoinRanking />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;

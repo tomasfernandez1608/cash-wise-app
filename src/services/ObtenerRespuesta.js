48e4db043bd34c42d3b32b8e13b32ac59ee5bbaf
soldiersnake <mariano.maciasgandulfo@gmail.com>
2023-11-06 21:46:37 -0300

    consulta chay aun sin funcionar erro 429

--- src/services/ObtenerRespuesta.js
+++ src/services/ObtenerRespuesta.js
@@ -1,27 +1,36 @@
-export async function obtenerChatRespuesta() {
+export async function obtenerChatRespuesta(consultaAlChat) {
+  const api_key = "";
 
-    const api_key = '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';
-    
-    try {
-        const res = await fetch('https://api.openai.com/v1/completions', {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${api_key}`,
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo-instruct',
-                prompt: 'Dame un ejemplo de programacion aleatoreo en algun lenguaje',
-                max_tokens: 10, // cantidad de palabras aprox contestadas
-            })
-        });
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-          }
-          console.log(await res.json());
-          // return await res.json();
-    } catch (error) {
-        console.error('Error fetching data: ', error);
+  const url = "https://api.openai.com/v1/completions"
+  
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + api_key,
+        // `Bearer ${api_key}`
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: `${consultaAlChat}, Dame un ejemplo de lenguajes de programacion para desarrollo web`,
+        max_tokens: 7, // cantidad de palabras aprox contestadas
+        temperature: 0,
+      }),
+    });
+    // if (res.status === 429) {
+    //     // Si obtienes el error 429, espera durante un tiempo y luego realiza la solicitud nuevamente.
+    //     await new Promise((resolve) => setTimeout(resolve, 5000)); // Espera 1 segundo
+    //     return obtenerChatRespuesta(consultaAlChat); // Llama a la función nuevamente con la misma consulta
+    //   }
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
     }
 
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
 }

a3f591ba36334d1ed3d5be73c4b50e02400440b6
tomasfernandez1608 <tomasfernandez1608@hotmail.com>
2023-11-09 16:25:35 -0300

    Ingresos Finalizado

--- src/components/ModalCrearIngreso.jsx
+++ src/components/ModalCrearIngreso.jsx
@@ -1,29 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 
-const ModalIngresos = ({ showModalIngresos, cerrarModal, ingresoAEditar }) => {
-  const [descripcion, setDescripcion] = useState('');
+const ModalCrearIngreso= ({ showModalCrearIngreso, cerrarModal }) => {
+  const [fuente, setFuente] = useState("");
+  const [descripcion, setDescripcion] = useState("");
   const [monto, setMonto] = useState('');
-  const [fuente, setFuente] = useState('');
-
+  const idusuario = JSON.parse(localStorage.getItem("user")).idusuario;
   const handleEditar = async () => {
     const data = {
-      id_ingreso: ingresoAEditar.id_ingreso,  
+        idusuario:idusuario,
       fuente:fuente,
       descripcion: descripcion,
       monto: monto,
     };
+    console.log(data);
     try {
-      const response = await fetch("http://localhost/serverWiseApp/editarIngreso.php", {
+      const response = await fetch("http://localhost/serverWiseApp/registrarIngreso.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
-      console.log(response);
+
       if (response.ok) {
+        const data = await response.json();
+        if (data.mensaje == "true") {
           cerrarModal();
+          window.location.reload();
+        } else {
+          console.error("Operacion Realizada");
+        }
       } else {
         console.error("Error en la solicitud al servidor.");
       }
@@ -31,26 +38,20 @@ const ModalIngresos = ({ showModalIngresos, cerrarModal, ingresoAEditar }) => {
       console.error("Error en la solicitud al servidor:", error);
     }
   };
-  useEffect(() => {
-    if (ingresoAEditar) {
-      setDescripcion(ingresoAEditar.descripcion || '');
-      setMonto(ingresoAEditar.monto || '');
-      setFuente(ingresoAEditar.fuente || '')
-    }
-  }, [ingresoAEditar])
+
   return (
     <div>
-      {showModalIngresos && (
+      {showModalCrearIngreso && (
         <div className="modal" tabIndex="-1" role="dialog" style={{ display: "block" }}>
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title">Editar Ingreso</h5>
+                <h5 className="modal-title">Crear Ingreso</h5>
                 <button type="button" className="btn-close" onClick={cerrarModal}></button>
               </div>
               <div className="modal-body">
                 <div className="form-group m-2">
-                  <label className="mb-2 " htmlFor="fuente">fuente:</label>
+                  <label className="mb-2 " htmlFor="fuente">Fuente:</label>
                   <input
                     type="text"
                     className="form-control mb-2"
@@ -70,12 +71,12 @@ const ModalIngresos = ({ showModalIngresos, cerrarModal, ingresoAEditar }) => {
                   />
                 </div>
                 <div className="form-group m-2">
-                  <label className="mb-2 " htmlFor="color">Monto:</label>
+                  <label className="mb-2 " htmlFor="monto">monto:</label>
                   <input
 
                     type="text"
                     className="form-control mb-2"
-                    id="Monto"
+                    id="monto"
                     value={monto}
                     onChange={(e) => setMonto(e.target.value)}
                   />
@@ -85,22 +86,21 @@ const ModalIngresos = ({ showModalIngresos, cerrarModal, ingresoAEditar }) => {
                 <button className="btn btn-secondary" onClick={cerrarModal}>
                   Cerrar
                 </button>
-                <button className="btn btn-primary" onClick={handleEditar}>Guardar cambios</button>
+                <button className="btn btn-primary" onClick={handleEditar}>Guardar Ingreso</button>
               </div>
             </div>
           </div>
         </div>
       )}
 
-      {showModalIngresos && <div className="modal-backdrop show"></div>}
+      {showModalCrearIngreso && <div className="modal-backdrop show"></div>}
     </div>
   );
 };
 
-ModalIngresos.propTypes = {
-  showModalIngresos: PropTypes.any.isRequired,
+ModalCrearIngreso.propTypes = {
+  showModalCrearIngreso: PropTypes.any.isRequired,
   cerrarModal: PropTypes.any.isRequired,
-  ingresoAEditar: PropTypes.any.isRequired
 };
 
-export default ModalIngresos;
+export default ModalCrearIngreso;
